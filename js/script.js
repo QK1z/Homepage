31075313826d6dc1345b6e81c27c1b700857af86
Pawel Kotlarek <pawelkotlarek85@gmail.com>
2023-02-22 22:04:01 +0100

    unusless files deleted

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 console.log("Witam i kłaniam się nisko ;)");
 
 let removeImageButton = document.querySelector(".js-removeImageButton");
-let image = document.querySelector(".header__image");
+let image = document.querySelector(".js-header__image");
 
 removeImageButton.addEventListener("click", () => {
     image.remove();
 });
 
 let otherBackground = document.querySelector(".js-otherBackground");
-let body = document.querySelector(".body");
-let theme = document.querySelector(".theme");
+let body = document.querySelector(".js-body");
+let theme = document.querySelector(".js-theme");
 
 otherBackground.addEventListener("click", () => {
-    body.classList.toggle("dark");
+    body.classList.toggle("body--dark");
 
-    if (body.classList.contains("dark")) {
+    if (body.classList.contains("body--dark")) {
         theme.innerText = "Jasny";
     } else {
         theme.innerText = "Ciemny";
